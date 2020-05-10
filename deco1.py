85e263de314e67ba82ce73861e2c7c0aabc0ee67
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-10 13:18:05 +0500

    add 1st example

--- deco1.py
+++ deco1.py
@@ -1,1 +1,17 @@
-print("hello world")
+def deco_func(any_func):
+    def wrp():
+        print("this is awesome")
+        any_func()
+    return wrp
+
+
+@deco_func
+def func1():
+    print("this is function 1 ")
+
+
+func1()
+
+
+def func2():
+    print("this is function 2 ")

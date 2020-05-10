5ad9a1fe0c51bf636651ca87c3da45d5b7f93e07
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-10 14:42:17 +0500

    add 2nd test

--- tesr.py
+++ tesr.py
@@ -2,22 +2,43 @@ from functools import wraps
 import time
 
 
-def time_calc(function):
-    @wraps(function)
+# def time_calc(function):
+#     @wraps(function)
+#     def wrap(*args, **kwargs):
+#         print(f"this function is {function.__name__} ")
+#         t1 = time.time()
+#         retuned = function(*args, **kwargs)
+#         t2 = time.time()
+#         total = t2 - t1
+#         print(f"this fuction took {total} seconds ")
+#         return retuned
+#     return wrap
+
+
+# @time_calc
+# def square(a):
+#     return a**2
+
+
+# print(square(100000))
+
+
+def add_int(fuction):
+    @wraps(fuction)
     def wrap(*args, **kwargs):
-        print(f"this function {function.__name__} name ")
-        t1 = time.time()
-        retuned = function(*args, **kwargs)
-        t2 = time.time()
-        total = t2 - t1
-        print(f"this fuction took {total} seconds ")
-        return retuned
+        if all([type(arg) == int for arg in args]):
+            return fuction(*args, **kwargs)
+        else:
+            print("invalid Entry")
     return wrap
 
 
-@time_calc
-def square(a):
-    return a**2
+@add_int
+def add(*args):
+    total = 0
+    for arg in args:
+        total += arg
+    return total
 
 
-square(100000)
+print(add(1, 2, 3, 4,[1,2,3,]))

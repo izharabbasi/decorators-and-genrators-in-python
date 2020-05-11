f543066ea4ef940c83200eba806f63b07dd780cb
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-11 10:58:41 +0500

    add 2nd example

--- gen_intro.py
+++ gen_intro.py
@@ -1,13 +1,24 @@
-def num(n):
+# def num(n):
+#     for i in range(1, n+1):
+#         yield i
+
+
+# numbers = num(10)
+
+# for i in numbers:
+#     print(i)
+
+
+# for i in numbers:
+#     print(i)
+
+def even(n):
     for i in range(1, n+1):
-        yield i
+        if i % 2 == 0:
+            yield i
 
 
-numbers = num(10)
+is_even = even(10)
 
-for i in numbers:
-    print(i)
-
-
-for i in numbers:
-    print(i)
+for num in is_even:
+    print(num)
